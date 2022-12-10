ef0bd76aa2520aa901c38e7c4c680cdf24398b52
RuolinLi06 <li.ruolin@northeastern.edu>
2022-12-10 17:57:16 -0500

    update model

---
+++ src/model/Goods.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Ruolin
+ */
+public class Goods {
+    private int goodsId;
+    private String goodsName;
+
+    public int getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(int goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" + "goodsId=" + goodsId + ", goodsName=" + goodsName + '}';
+    }
+
+}
